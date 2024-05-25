1f31f35ebe9922c4af06d11a0c09bcdb0e7c3f71
Hikam-Baidawih <hikambaidawih@gmail.com>
2024-05-25 17:09:30 +0700

    menambah halaman web di index.php

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Pendaftaran Pasien Baru | RS Santoso</title>
+</head>
+
+<body>
+    <header>
+        <h3>Pendaftaran Pasien Baru</h3>
+        <h1>RS Santoso</h1>
+    </header>
+
+    <h4>Menu</h4>
+    <nav>
+        <ul>
+            <li><a href="form-daftar.php">Daftar Baru</a></li>
+            <li><a href="list-pasien.php">Data Pasien</a></li>
+        </ul>
+    </nav>
+
+</body>
+
+</html>
